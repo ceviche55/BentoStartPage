5cd242d9c5dbf73c634043d67664c866f76d4ada
Khalid <khalid.hamu@gmail.com>
2023-09-04 14:40:37 -0600

    small tweaks

--- config.js
+++ config.js
@@ -6,225 +6,225 @@
 // └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
 
 const CONFIG = {
-	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
-	// ├┴┐├─┤└─┐││  └─┐
-	// └─┘┴ ┴└─┘┴└─┘└─┘
+    // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+    // ├┴┐├─┤└─┐││  └─┐
+    // └─┘┴ ┴└─┘┴└─┘└─┘
 
-	// General
-	name: 'Khalid',
-	imageBackground: true,
-	openInNewTab: true,
-	twelveHourFormat: true,
+    // General
+    name: 'Khalid',
+    imageBackground: true,
+    openInNewTab: false,
+    twelveHourFormat: true,
 
-	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+    // Greetings
+    greetingMorning: 'Good morning!',
+    greetingAfternoon: 'Good afternoon,',
+    greetingEvening: 'Good evening,',
+    greetingNight: 'Go to Sleep!',
 
-	// Layout
-	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
+    // Layout
+    bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
-	// Weather
-	weatherKey: 'eb70acfa31a26bca9b7dcdd5ce2faf17', // Write here your API Key
-	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'F', // 'F', 'C'
-	language: 'en', // More languages in https://openweathermap.org/current#multi
+    // Weather
+    weatherKey: 'eb70acfa31a26bca9b7dcdd5ce2faf17', // Write here your API Key
+    weatherIcons: 'Onedark', // 'Onedark', 'Nord', 'Dark', 'White'
+    weatherUnit: 'F', // 'F', 'C'
+    language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '39.7392',
-	defaultLongitude: '104.9903',
+    trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+    defaultLatitude: '39.7392',
+    defaultLongitude: '104.9903',
 
-	// Autochange
-	autoChangeTheme: true,
+    // Autochange
+    autoChangeTheme: true,
 
-	// Autochange by OS
-	changeThemeByOS: true,
+    // Autochange by OS
+    changeThemeByOS: true,
 
-	// Autochange by hour options (24hrs format, string must be in: hh:mm)
-	changeThemeByHour: false,
-	hourDarkThemeActive: '18:30',
-	hourDarkThemeInactive: '07:00',
+    // Autochange by hour options (24hrs format, string must be in: hh:mm)
+    changeThemeByHour: false,
+    hourDarkThemeActive: '18:30',
+    hourDarkThemeInactive: '07:00',
 
-	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
-	// ├┴┐│ │ │  │ │ ││││└─┐
-	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+    // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+    // ├┴┐│ │ │  │ │ ││││└─┐
+    // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
 
-	firstButtonsContainer: [
-		{
-			id: '1',
-			name: 'Gmail',
-			icon: 'mail',
-			link: 'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox',
-		},
-		{
-			id: '2',
-			name: 'Canvas',
-			icon: 'backpack',
-			link: 'https://ucdenver.instructure.com/',
-		},
-		{
-			id: '3',
-			name: 'Youtube',
-			icon: 'youtube',
-			link: 'https://www.youtube.com/',
-		},
-		{
-			id: '4',
-			name: 'G Drive',
-			icon: 'hard-drive',
-			link: 'https://drive.google.com/drive/u/0/my-drive',
-		},
-		{
-			id: '5',
-			name: 'Medium',
-			icon: 'newspaper',
-			link: 'https://medium.com/',
-		},
-		{
-			id: '6',
-			name: 'Home Assistent',
-			icon: 'home',
-			link: 'http://homeassistant.local:8123/lovelace/default_view',
-		},
-	],
+    firstButtonsContainer: [
+        {
+            id: '1',
+            name: 'Gmail',
+            icon: 'mail',
+            link: 'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox',
+        },
+        {
+            id: '2',
+            name: 'Canvas',
+            icon: 'backpack',
+            link: 'https://ucdenver.instructure.com/',
+        },
+        {
+            id: '3',
+            name: 'Youtube',
+            icon: 'youtube',
+            link: 'https://www.youtube.com/',
+        },
+        {
+            id: '4',
+            name: 'G Drive',
+            icon: 'hard-drive',
+            link: 'https://drive.google.com/drive/u/0/my-drive',
+        },
+        {
+            id: '5',
+            name: 'Medium',
+            icon: 'newspaper',
+            link: 'https://medium.com/',
+        },
+        {
+            id: '6',
+            name: 'Home Assistent',
+            icon: 'home',
+            link: 'http://homeassistant.local:8123/lovelace/default_view',
+        },
+    ],
 
-	secondButtonsContainer: [
-		{
-			id: '1',
-			name: 'Neetcode',
-			icon: 'code',
-			link: 'https://neetcode.io/',
-		},
-		{
-			id: '2',
-			name: 'EdClub',
-			icon: 'bot',
-			link: 'https://www.edclub.com/sportal/program-3.game',
-		},
-		{
-			id: '3',
-			name: 'Amazon',
-			icon: 'shopping-bag',
-			link: 'https://amazon.com/',
-		},
-		{
-			id: '4',
-			name: 'MonkeyType',
-			icon: 'keyboard',
-			link: 'https://monkeytype.com/',
-		},
-		{
-			id: '5',
-			name: 'Disney+',
-			icon: 'tv',
-			link: 'https://www.disneyplus.com/',
-		},
-		{
-			id: '6',
-			name: 'CrunchyRoll',
-			icon: 'tv-2',
-			link: 'https://beta.crunchyroll.com/',
-		},
-	],
+    secondButtonsContainer: [
+        {
+            id: '1',
+            name: 'Neetcode',
+            icon: 'code',
+            link: 'https://neetcode.io/',
+        },
+        {
+            id: '2',
+            name: 'EdClub',
+            icon: 'bot',
+            link: 'https://www.edclub.com/sportal/program-3.game',
+        },
+        {
+            id: '3',
+            name: 'Amazon',
+            icon: 'shopping-bag',
+            link: 'https://amazon.com/',
+        },
+        {
+            id: '4',
+            name: 'MonkeyType',
+            icon: 'keyboard',
+            link: 'https://monkeytype.com/',
+        },
+        {
+            id: '5',
+            name: 'Disney+',
+            icon: 'tv',
+            link: 'https://www.disneyplus.com/',
+        },
+        {
+            id: '6',
+            name: 'CrunchyRoll',
+            icon: 'tv-2',
+            link: 'https://beta.crunchyroll.com/',
+        },
+    ],
 
-	// ┬  ┬┌─┐┌┬┐┌─┐
-	// │  │└─┐ │ └─┐
-	// ┴─┘┴└─┘ ┴ └─┘
+    // ┬  ┬┌─┐┌┬┐┌─┐
+    // │  │└─┐ │ └─┐
+    // ┴─┘┴└─┘ ┴ └─┘
 
-	// First Links Container
-	firstlistsContainer: [
-		{
-			icon: 'music',
-			id: '1',
-			links: [
-				{
-					name: 'Inspirational',
-					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-				},
-				{
-					name: 'Classic',
-					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-				},
-				{
-					name: 'Oldies',
-					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-				},
-				{
-					name: 'Rock',
-					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-				},
-			],
-		},
-		{
-			icon: 'coffee',
-			id: '2',
-			links: [
-				{
-					name: 'Linkedin',
-					link: 'https://www.linkedin.com',
-				},
-				{
-					name: 'Dribbble',
-					link: 'https://www.dribbble.com',
-				},
-				{
-					name: 'Trello',
-					link: 'https://www.trello.com',
-				},
-				{
-					name: 'Slack',
-					link: 'https://www.slack.com',
-				},
-			],
-		},
-	],
+    // First Links Container
+    firstlistsContainer: [
+        {
+            icon: 'music',
+            id: '1',
+            links: [
+                {
+                    name: 'Inspirational',
+                    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+                },
+                {
+                    name: 'Classic',
+                    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+                },
+                {
+                    name: 'Oldies',
+                    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+                },
+                {
+                    name: 'Rock',
+                    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+                },
+            ],
+        },
+        {
+            icon: 'coffee',
+            id: '2',
+            links: [
+                {
+                    name: 'Linkedin',
+                    link: 'https://www.linkedin.com',
+                },
+                {
+                    name: 'Dribbble',
+                    link: 'https://www.dribbble.com',
+                },
+                {
+                    name: 'Trello',
+                    link: 'https://www.trello.com',
+                },
+                {
+                    name: 'Slack',
+                    link: 'https://www.slack.com',
+                },
+            ],
+        },
+    ],
 
-	// Second Links Container
-	secondListsContainer: [
-		{
-			icon: 'binary',
-			id: '1',
-			links: [
-				{
-					name: 'Spotify',
-					link: 'https://www.spotify.com',
-				},
-				{
-					name: 'Reddit',
-					link: 'https://www.reddit.com',
-				},
-				{
-					name: 'Hashnode',
-					link: 'https://www.hashnode.com',
-				},
-				{
-					name: 'Pocket',
-					link: 'https://www.pocket.com',
-				},
-			],
-		},
-		{
-			icon: 'github',
-			id: '2',
-			links: [
-				{
-					name: 'Front',
-					link: 'https://www.reddit.com/r/Frontend/',
-				},
-				{
-					name: 'Rust',
-					link: 'https://www.reddit.com/r/rust/',
-				},
-				{
-					name: 'Go',
-					link: 'https://www.reddit.com/r/golang/',
-				},
-				{
-					name: 'Repos',
-					link: 'https://github.com/migueravila',
-				},
-			],
-		},
-	],
+    // Second Links Container
+    secondListsContainer: [
+        {
+            icon: 'binary',
+            id: '1',
+            links: [
+                {
+                    name: 'Spotify',
+                    link: 'https://www.spotify.com',
+                },
+                {
+                    name: 'Reddit',
+                    link: 'https://www.reddit.com',
+                },
+                {
+                    name: 'Hashnode',
+                    link: 'https://www.hashnode.com',
+                },
+                {
+                    name: 'Pocket',
+                    link: 'https://www.pocket.com',
+                },
+            ],
+        },
+        {
+            icon: 'github',
+            id: '2',
+            links: [
+                {
+                    name: 'Front',
+                    link: 'https://www.reddit.com/r/Frontend/',
+                },
+                {
+                    name: 'Rust',
+                    link: 'https://www.reddit.com/r/rust/',
+                },
+                {
+                    name: 'Go',
+                    link: 'https://www.reddit.com/r/golang/',
+                },
+                {
+                    name: 'Repos',
+                    link: 'https://github.com/migueravila',
+                },
+            ],
+        },
+    ],
 };
